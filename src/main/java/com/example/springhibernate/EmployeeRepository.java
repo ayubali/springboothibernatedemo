606af5f5354f65d5c283d29ddbcdf546775d6215
ayubali <ayub.ali.sarker@gmail.com>
2022-02-21 08:27:14 -0800

    first commit

---
+++ src/main/java/com/example/springhibernate/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.springhibernate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+ 
+@Repository
+public interface EmployeeRepository 
+        extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
